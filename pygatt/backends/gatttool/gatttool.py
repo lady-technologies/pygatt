a93d2c18771c9f57fc3954753218a5dc5d7d7329
Jakub Hrabec <xhrabec@gmail.com>
2019-01-15 13:02:26 +0100

    hackfix empty handle value throwing exception

--- pygatt/backends/gatttool/gatttool.py
+++ pygatt/backends/gatttool/gatttool.py
@@ -532,9 +532,12 @@ class GATTToolBackend(BLEBackend):
         """
         with self._receiver.event("value/descriptor", timeout=timeout):
             self.sendline('char-read-hnd %s' % handle)
-        rval = self._receiver.last_value("value/descriptor", "after"
-                                         ).split()[1:]
-        return bytearray([int(x, 16) for x in rval])
+        try:
+            rval = self._receiver.last_value("value/descriptor", "after"
+                                             ).split()[1:]
+            return bytearray([int(x, 16) for x in rval])
+        except ValueError:
+            return bytearray()
 
     def reset(self):
         subprocess.Popen(["sudo", "systemctl", "restart", "bluetooth"]).wait()
